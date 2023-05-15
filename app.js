930a29d269f07323822f495d0dd40b6a65e74156
ThomasLonjon <lonjon.thomas@gmail.com>
2023-05-15 17:57:06 +0200

    added users

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const database = require("./database.js");
 const movieHandlers = require("./movieHandlers");
-app.use(express.json());
+const userHandlers = require("./userHandlers");
 const port = process.env.APP_PORT ?? 5001;
+app.use(express.json());
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
-app.get("/", welcome);
-
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-
+// ----------------------------Connection to server--------------------------
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
@@ -23,3 +18,15 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+// ----------------------------Routes Welcome--------------------------
+app.get("/", welcome);
+
+// ----------------------------Routes Movies--------------------------
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies", movieHandlers.postMovie);
+
+// ----------------------------Routes Users--------------------------
+
+app.get("/api/users", userHandlers.getUsers);
+app.post("/api/users", userHandlers.postUser);
---
+++ userHandlers.js
@@ -0,0 +1,41 @@
+const database = require("./database.js");
+
+// ---------------------------------------getUsers----------------------------------------
+
+const getUsers = (req, res) => {
+  database
+    .query("select * from users")
+    .then(([user]) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+// ---------------------------------------PostUsers----------------------------------------
+
+const postUser = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the user");
+    });
+};
+
+// ---------------------------------------EXPORTS----------------------------------------
+
+module.exports = {
+  getUsers,
+  postUser,
+};
