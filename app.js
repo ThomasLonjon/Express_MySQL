eda4de13af657112409d15e7574d439720dc247d
ThomasLonjon <lonjon.thomas@gmail.com>
2023-06-20 09:56:10 +0200

    added JWT and authentification wall

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 require("dotenv").config();
 
 const express = require("express");
-
-const { hashPassword } = require("./auth.js");
-
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
 const app = express();
-
 app.use(express.json());
-
 const port = process.env.APP_PORT ?? 5000;
+
+// ---------------------------------------- Home----------------------------------------
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -16,21 +14,32 @@ const welcome = (req, res) => {
 
 app.get("/", welcome);
 
+// ---------------------------------------- Handlers----------------------------------------
+
 const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
+
+// ---------------------------------------- public routes----------------------------------------
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+
+app.post("/api/login", userHandlers.getUserByEmailWithPasswordAndPassToNext, verifyPassword);
+app.post("/api/users", hashPassword, userHandlers.postUser);
+
+// ---------------------------------------- routes to protect----------------------------------------
+
+app.use(verifyToken); // authentication wall : verifyToken is activated for each route after this line
+
 app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-
-const userHandlers = require("./userHandlers");
-
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", hashPassword, userHandlers.postUser);
 app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
+
+// ---------------------------------------- Listen Error----------------------------------------
 
 app.listen(port, (err) => {
   if (err) {
